011b8aff85b6685371cabcef90b5b144c7eac28e
Bart Wijnants <bartwijnants@outlook.com>
2013-06-27 11:20:24 +0200

    refactored and now you can add rows
    Refactored using an oop aproach and a subviewmodel

--- koGrid.js
+++ koGrid.js
@@ -1,100 +1,182 @@
-Element.prototype.createKoGrid = function(parameterObject) {
-    var htmlString = "";
+Element.prototype.createKoGrid = function (parameterObject) {
+    if (parameterObject.hasOwnProperty("viewModel") &&
+        parameterObject.hasOwnProperty("data") &&
+        parameterObject.hasOwnProperty("columns")) {
 
-    if (parameterObject.hasOwnProperty("viewModel")) {
-        parameterObject.viewModel["koGridSortBy"] = { prop: ko.observable(""), direction: ko.observable("") };
+        var subModelBase = "koGrid";
+        var suffix = 1
 
-        if (parameterObject.hasOwnProperty("data") && parameterObject.hasOwnProperty("columns")) {
-            //Create table
-            htmlString += '<table><tbody>';
-
-            //Add header row
-            htmlString += createHeaderRow(parameterObject.data, parameterObject.columns);
-
-            //Add content
-            var allowDelete = true;
-            if (parameterObject.hasOwnProperty("delete") && parameterObject.delete == false) {
-                allowDelete = false;
-            }
-
-            htmlString += createContentTemplate(parameterObject.data, parameterObject.columns, allowDelete);
-
-            htmlString += '</tbody></table>';
+        while (parameterObject.viewModel.hasOwnProperty(subModelBase + suffix.toString())) {
+            suffix = suffix + 1;
         }
-    }
 
-    this.innerHTML = htmlString;
+        var subModelName = subModelBase + suffix.toString();
+
+        parameterObject.viewModel[subModelName] = new koGridViewModel(subModelName, parameterObject);
+
+        this.innerHTML = parameterObject.viewModel[subModelName].getHtml();
+    } else {
+        throw new koGridException("incomplete parameters");
+        return;
+    }
 }
 
-function createHeaderRow(data, columns) {
-    var headerRow = "";
+function koGridViewModel(model, parameterObject) {
+    var self = this;
 
-    headerRow += "<tr>";
-    for (var i=0; i < columns.length; i++)
-    {
-        var column = columns[i];
+    var columns = parameterObject.columns;
+    var data = parameterObject.data;
+    var allowDelete = true;
 
-        if (column.hasOwnProperty("header")) {
-            if (column.hasOwnProperty("prop")) {
-                headerRow += '<td>';
-                headerRow += '<a data-bind="click: koGridSort.bind($data, ' + data +', \'' + column.prop + '\')">' + column.header;
-                headerRow += '<span style="display: none" data-bind="visible: koGridSortBy.prop() == \'' + column.prop + '\' && koGridSortBy.direction() == \'up\'">&#x25B2;</span>';
-                headerRow += '<span style="display: none" data-bind="visible: koGridSortBy.prop() == \'' + column.prop + '\' && koGridSortBy.direction() == \'down\'">&#x25BC;</span>';
-                headerRow += '</td>';
-            } else {
-                headerRow += "<td>" + column.header + "</td>";
-            }
+    self.sortBy = { prop: ko.observable(""), direction: ko.observable(0) };
 
-        }
-    }
-    headerRow += "</tr>";
-
-    return headerRow;
-}
-
-function createContentTemplate(data, columns, allowDelete) {
-    var contentTemplate = "";
-
-    contentTemplate += "<!-- ko foreach: " + data + " -->";
-
-    contentTemplate += "<tr>";
-    for (var i=0; i < columns.length; i++)
-    {
+    for (var i=0; i < columns.length; i++) {
         var column = columns[i];
 
         if (column.hasOwnProperty("prop")) {
-            if (column.hasOwnProperty("edit") && column.edit == false) {
-                contentTemplate += '<td data-bind="text: ' + column.prop + '"></td>';
-            } else if (column.hasOwnProperty("binding")) {
-                contentTemplate += '<td><input data-bind="' + column.binding + ': ' + column.prop + '" /></td>';
+            self[column.prop] = ko.observable("");
+        }
+    }
+
+    self.sort = function (data, property) {
+        var oldProp = self.sortBy.prop();
+        self.sortBy.prop(property);
+
+        if (self.sortBy.direction() == 0 || (oldProp != property) || self.sortBy.direction() == -1) {
+            self.sortBy.direction(1);
+        } else {
+            self.sortBy.direction(-1);
+        }
+
+        sortData(data);
+    }
+
+    self.delete = function (data, index) {
+        data.splice(index, 1);
+    }
+
+    self.add = function (data) {
+        var newRow = new Object();
+
+        for (var i=0; i < columns.length; i++) {
+            var column = columns[i];
+
+            if (column.hasOwnProperty("prop")) {
+                newRow[column.prop] = ko.observable(self[column.prop]());
+                self[column.prop]("");
+            }
+        }
+
+        data.push(newRow);
+
+        sortData(data);
+    }
+
+    self.getHtml = function () {
+        var htmlString = "";
+
+        //Create table
+        htmlString += '<table><tbody>';
+
+        //Add header row
+        htmlString += createHeaderRow();
+
+        //Add content
+        htmlString += createContentTemplate();
+
+        //Add add row
+        htmlString += createAddRow();
+
+        htmlString += '</tbody></table>';
+
+        return htmlString;
+    }
+
+    var createHeaderRow = function () {
+        var headerRow = "";
+
+        headerRow += "<tr>";
+        for (var i=0; i < columns.length; i++) {
+            var column = columns[i];
+
+            if (column.hasOwnProperty("header")) {
+                if (column.hasOwnProperty("prop")) {
+                    headerRow += '<td>';
+                    headerRow += '<a data-bind="click: ' + model + '.sort.bind($data, ' + data +', \'' + column.prop + '\')">' + column.header;
+                    headerRow += '<span style="display: none" data-bind="visible: ' + model + '.sortBy.prop() == \'' + column.prop + '\' && ' + model + '.sortBy.direction() == 1">&#x25B2;</span>';
+                    headerRow += '<span style="display: none" data-bind="visible: ' + model + '.sortBy.prop() == \'' + column.prop + '\' && ' + model + '.sortBy.direction() == -1">&#x25BC;</span>';
+                    headerRow += '</td>';
+                } else {
+                    headerRow += "<td>" + column.header + "</td>";
+                }
+
+            }
+        }
+        headerRow += "</tr>";
+
+        return headerRow;
+    }
+
+    var createContentTemplate = function () {
+        var contentTemplate = "";
+
+        contentTemplate += "<!-- ko foreach: " + data + " -->";
+
+        contentTemplate += "<tr>";
+        for (var i=0; i < columns.length; i++) {
+            var column = columns[i];
+
+            if (column.hasOwnProperty("prop")) {
+                if (column.hasOwnProperty("edit") && column.edit == false) {
+                    contentTemplate += '<td data-bind="text: ' + column.prop + '"></td>';
+                } else if (column.hasOwnProperty("binding")) {
+                    contentTemplate += '<td><input data-bind="' + column.binding + ': ' + column.prop + '" /></td>';
+                } else {
+                    contentTemplate += '<td><input data-bind="value: ' + column.prop + '" /></td>';
+                }
+            }
+        }
+        if (allowDelete) {
+            contentTemplate += '<td><a data-bind="click: $parent.' + model + '.delete.bind($data, $parent[\'' + data + '\'], $index())">Delete</a></td>';
+        }
+        contentTemplate += "</tr>";
+
+        contentTemplate += "<!-- /ko -->";
+
+        return contentTemplate;
+    }
+
+    var createAddRow = function() {
+        var addRow = "";
+
+        addRow += "<tr>";
+
+        for (var i=0; i < columns.length; i++) {
+            var column = columns[i];
+            addRow += '<td><input data-bind="value: ' + model + '.' + column.prop + '" /></td>';
+        }
+
+        addRow += '<td><a data-bind="click: ' + model + '.add.bind($data, ' + data + ')">Add</a></td>';
+
+        addRow += "</tr>";
+
+        return addRow;
+    }
+
+    var sortData = function (data) {
+        if (self.sortBy.prop().length > 0 && self.sortBy.direction() != 0) {
+            var property = self.sortBy.prop();
+
+            if (self.sortBy.direction() == 1) {
+                data.sort(function(left, right) { return left[property]() == right[property]() ? 0 : (left[property]() < right[property]() ? -1 : 1) });
             } else {
-                contentTemplate += '<td><input data-bind="value: ' + column.prop + '" /></td>';
+                data.sort(function(left, right) { return left[property]() == right[property]() ? 0 : (left[property]() < right[property]() ? 1 : -1) });
             }
         }
     }
-    if (allowDelete) {
-        contentTemplate += '<td><a data-bind="click: koGridDelete.bind($data, $parent[\'' + data + '\'], $index())">Delete</a></td>';
-    }
-    contentTemplate += "</tr>";
-
-    contentTemplate += "<!-- /ko -->";
-
-    return contentTemplate;
 }
 
-function koGridSort(data, property) {
-    var oldProp = this.koGridSortBy.prop();
-    this.koGridSortBy.prop(property);
-
-    if (this.koGridSortBy.direction() == "" || (oldProp != property) || this.koGridSortBy.direction() == "down") {
-        this.koGridSortBy.direction("up");
-        data.sort(function(left, right) { return left[property]() == right[property]() ? 0 : (left[property]() < right[property]() ? -1 : 1) });
-    } else {
-        this.koGridSortBy.direction("down");
-        data.sort(function(left, right) { return left[property]() == right[property]() ? 0 : (left[property]() < right[property]() ? 1 : -1) });
-    }
-}
-
-function koGridDelete(data, index) {
-    data.splice(index, 1);
+function koGridException(message) {
+    this.message = message;
+    this.name = "koGridException";
 }
